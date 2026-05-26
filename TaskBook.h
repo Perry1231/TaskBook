c9deb30df1ad0352779d93fe3e157a23267540c3
Vladyslav <bravilmaksim@gmail.com>
2026-05-26 20:03:19 +0300

    Update 1.0.8

--- TaskBook.h
+++ TaskBook.h
@@ -1,12 +1,13 @@
 #ifndef TASKBOOK_H
 #define TASKBOOK_H
-
+#pragma once 
 #include <string>
 
-class TaskBook
+
+class TaskBook 
 {
-public:
-    class Task
+    public:
+    class Task 
     {
     public:
         std::string name;
@@ -14,31 +15,24 @@ public:
         int num;
         int priority;
         bool completed;
+ 
+        Task() : num(0), priority(0), completed(false) {}                                                                   // Default constructor
 
-        Task();
-        Task(const std::string& name_, const std::string& description_, int num_, int priority_);
+        Task(const std::string& name_, const std::string& description_, int num_, int priority_)                            // Parameterized constructor    
+            : name(name_), description(description_), num(num_), priority(priority_), completed(false) {}
     };
 
-    static int taskCount;
 
-    TaskBook();
-    ~TaskBook();
-
-    void AddTask(const std::string& name_, const std::string& description_, int priority_);
-    void RemoveTask();
-    void DisplayTasks();
-    void SortTasks();
-    void FilterTasks();
-    void OverrideTask();
-    void MarkTaskCompleted();
-
-private:
-    Task* tasks;
-    int capacity;
-    int size;
-
-    void resize();
-};
+void DisplayTasks();
+void SortTasks();
+void FilterTasks();
+void OverrideTask();
+void ChoiceTask();
+void RemoveTask();
+void AddTask(const std::string& name_, const std::string& description_, int priority_);
+void DisplayMenu();
+void MarkTaskCompleted();
+ 
 
 void HelpFunction();
 void DisplayMenu();
