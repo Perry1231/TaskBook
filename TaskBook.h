8ef636294f9f2cfa8aefe39bf3f02489ec6b063c
Vladyslav <bravilmaksim@gmail.com>
2026-05-31 19:42:51 +0300

    Fix 15

--- TaskBook.h
+++ TaskBook.h
@@ -1,7 +1,8 @@
 #ifndef TASKBOOK_H
 #define TASKBOOK_H
 #include <string>
-#include <vector>
+#include <iostream>
+
 
 class TaskBook 
 {
@@ -15,17 +16,20 @@ class TaskBook
         int priority;
         bool completed;
  
-        Task() : num(0), priority(0), completed(false) {}
+        Task() : num(0), priority(0), completed(false) , description(0), name(0){}                                          // Default constructor
 
-        Task(const std::string& name_, const std::string& description_, int num_, int priority_)
+        Task(const std::string& name_, const std::string& description_, int num_, int priority_)                            // Parameterized constructor    
             : name(name_), description(description_), num(num_), priority(priority_), completed(false) {}
 
              static int taskCount;
+
     };
     public:
     
-    TaskBook() = default;
-
+    TaskBook();
+    ~TaskBook();
+        
+void Resize();
 void DisplayTasks();
 void SortTasks();
 void FilterTasks();
@@ -34,9 +38,13 @@ void ChoiceTask();
 void RemoveTask();
 void AddTask(const std::string& name_, const std::string& description_, int priority_);
 void MarkTaskCompleted();
+void CopyTask();
+TaskBook &operator = (const TaskBook &other);
 
  private:
-    std::vector<Task> tasks;
+    Task* tasks;
+    int capacity;                   //This variable keeps track of the current capacity of the tasks array, allowing for dynamic resizing when adding new tasks.
+    int size;                       //This variable keeps track of the current number of tasks in the task book, allowing for proper management of the tasks array and ensuring that operations like adding, removing, and displaying tasks are performed correctly based on the actual number of tasks present.
 };
 
 
