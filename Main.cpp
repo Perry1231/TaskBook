a094fe9e38652cbcc632e8768a7683dbfa7ffc43
Vladyslav <bravilmaksim@gmail.com>
2026-06-03 17:05:44 +0300

    Fix 23 (Suplementary commit)

---
+++ Main.cpp
@@ -0,0 +1,79 @@
+#include "TaskBook.h"
+#include <iostream>
+#include <limits>
+#include <string>
+
+int main()
+{
+    int choice;  
+    TaskBook taskBook;
+
+    while(true)
+    {
+        DisplayMenu();
+
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');
+            std::cout << "Invalid input! Please enter a number." << std::endl;
+            continue;
+        }
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch(choice) {
+        case 1: {
+            std::string name, description;
+            int priority=0;
+            std::cout << "Enter task name: ";
+            std::getline(std::cin, name);
+            std::cout << "Enter task description: ";
+            std::getline(std::cin, description);
+            std::cout << "Enter task priority: ";
+            std::cin >> priority;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(10000, '\n');
+                std::cout << "Invalid priority." << std::endl;
+                break;
+            }
+            taskBook.AddTask(name, description, priority);                  //Ready
+            break;
+        }
+        case 2:
+            taskBook.RemoveTask();                                          //Ready
+            break;
+        case 3:
+            taskBook.DisplayTasks();                                        //Ready         
+            break;
+        case 4:
+            taskBook.MarkTaskCompleted();                                   //Ready
+            break;      
+        case 5:
+            taskBook.CopyTask();                                            //Ready
+            break;
+        case 6:
+            taskBook.FilterTasks();                                         //Ready
+            break;
+        case 7: 
+            taskBook.OverrideTask();                                        //Ready
+            break;
+        case 8:
+            taskBook.ChoiceTask();                                          //Ready         
+            break;
+        case 9:
+            HelpFunction();                                                 //Ready
+            break;
+        case 0:
+            std::cout << "Exiting..." << std::endl;
+            return 0;
+        default:
+            std::cout << "Invalid choice. Please try again." << std::endl;
+        }
+    }
+
+    return 0;
+}
